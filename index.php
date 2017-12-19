b7beb0f4160f61541e9486455d030c0295547a24
11113127 <s11113127@stu.edu.tw>
2017-12-19 12:47:38 +0800

    add function event

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>EventSource</title>
-</head>
-<body>
-    <h3>Hello</h3>
+  <head>
+    <title>EventSource</title>
+  </head>
+  <body>
+    <h2>Hello</h2>
     <div id="result"></div>
 
     <script>
-        if(typeof(EventSource) !== "undefined") {
-            var source = new EventSource("service.php");
-            source.onmessage = function(event) {
-                document.getElementById("result").innerHTML += "<div>"+JSON.parse(event.data).hello+"</div>";
-                console.log(JSON.parse(event.data).hello)
-            };
-        } else {
-            document.getElementById("result").innerHTML = "瀏覽器未支援Event Source";
-        }
+      if(typeof(EventSource) !== "undefined") {
+        var source = new EventSource("service.php");
+
+        source.onmessage = function(event) {
+          document.getElementById("result").innerHTML += "<div>"+JSON.parse(event.data).HTML+"</div>";
+          console.log(JSON.parse(event.data).HTML);
+        };
+
+        source.addEventListener("CSS", function(event) {
+          document.getElementById("result").innerHTML += "<div>"+JSON.parse(event.data).CSS+"</div>";
+          console.log(JSON.parse(event.data).CSS);
+        });
+
+        source.addEventListener("JS", function(event) {
+          document.getElementById("result").innerHTML += "<div>"+JSON.parse(event.data).JS+"</div>";
+          console.log(JSON.parse(event.data).JS);
+        });
+
+        source.addEventListener("Format", function(event) {
+          document.getElementById("result").innerHTML += "<div>"+JSON.parse(event.data).XML+" and "+JSON.parse(event.data).JSON+"</div>";
+          console.log(JSON.parse(event.data).XML+" and "+JSON.parse(event.data).JSON);
+        });
+      } else {
+        document.getElementById("result").innerHTML = "瀏覽器未支援Event Source";
+      }
     </script>
-</body>
+  </body>
 </html>
--- service.php
+++ service.php
@@ -1,11 +1,20 @@
 <?php
-    header('Content-Type: text/event-stream');
-    header('Cache-Control: no-cache');
+    header("Content-Type: text/event-stream");
+    header("Cache-Control: no-cache");
 
-    $num = "world";
-    echo "data: {\"hello\":\"$num\"}".PHP_EOL;
+    echo "data: {\"HTML\": \"Hypertext Markup Language\"}\n\n";
+
+    echo "event: CSS\n";
+    echo "data: {\"CSS\": \"Cascading Style Sheets\"}\n\n";
+
+    echo "event: JS\n";
+    echo "data: {\"JS\": \"JavaScript\"}\n\n";
+
+    echo "event: Format\n";
+    echo "data: {\"XML\": \"Extensible Markup Language\" , \"JSON\": \"JavaScript Object Notation\"}\n\n";
+
     echo PHP_EOL;
     ob_flush();
     flush();
-    usleep(9600000);//10秒(前端花400毫秒)
+    usleep(9600000);//10秒(前端花0.4秒)
 ?>
